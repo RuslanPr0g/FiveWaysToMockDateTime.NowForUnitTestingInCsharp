f55084f545631ebce662474730cef17dc594c2aa
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-09-22 14:04:57 +0300

    The property Now must be virtual.
    Otherwise, the Moq framework will not be able to create the wrapper over the mocked type. We pollute the application logic with what is needed only for unit testing purposes.

--- Application/Providers/OnelineWrapperDateTimeProvider.cs
+++ Application/Providers/OnelineWrapperDateTimeProvider.cs
@@ -4,6 +4,11 @@ namespace Application.Providers
 {
     public class OnelineWrapperDateTimeProvider
     {
+        /// <summary>
+        /// The property Now must be virtual.
+        /// Otherwise, the Moq framework will not be able to create the wrapper over the mocked type.
+        /// We pollute the application logic with what is needed only for unit testing purposes.
+        /// </summary>
         public virtual DateTime Now { get; } = DateTime.Now;
     }
 }
